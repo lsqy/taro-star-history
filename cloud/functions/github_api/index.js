f6d0479278592a9639a91a08b877f9fb2bfecf6a
lsqy <lsqy93@gmail.com>
2019-11-03 22:24:02 +0800

    feat(cloud):修改云函数中请求逻辑

--- cloud/functions/github_api/index.js
+++ cloud/functions/github_api/index.js
@@ -1,41 +1,28 @@
 const cloud = require('wx-server-sdk')
-const axios = require('axios')
-const CircularJSON = require('circular-json');
+const rp = require('request-promise')
 const token = 'be6cf78904e54f1a5c265535c871354f818570f2'
 
 cloud.init({
   env: cloud.DYNAMIC_CURRENT_ENV
 })
 
-const axiosInstance = axios.create({
-    baseURL: 'https://api.github.com',
-    headers: {
-        Accept: 'application/vnd.github.v3.star+json',
-        Authorization: `token ${token}`
-    }
-})
-
-axios.interceptors.response.use((response) => {
-    // Do something with response data
-    let json = CircularJSON.stringify(response);
-    const jsonData = JSON.parse(JSON.stringify(json))
-    // let json = JSON.stringify(JSON.decycle(response));
-    // const jsonData = JSON.retrocycle(JSON.parse(response));
-    res.send(jsonData);
-    // res.send(response)
-}, function (error) {
-    // Do something with response error
-    return Promise.reject(error);
-});
-
-
 exports.main = async (event, context) => {
   console.log('event', event)
   console.log('context', context)
-//   const wxContext = cloud.getWXContext()
-  const stargazersUrl = `/repos/${event.repo}/stargazers`
-  const starRes = await axiosInstance.get(stargazersUrl)
+  const star_params = {
+    method: 'GET',
+    url: `https://api.github.com/repos/${event.repo}/stargazers`,
+    qs: {
+    },
+    headers: {
+      'Accept': 'application/vnd.github.v3.star+json',
+      'User-Agent': 'taro-star-history',
+      // Authorization: `token ${token}`
+    },
+    json: true
+  };
+  const starRes = await rp(star_params)
   return {
-    list: starRes.data
+    list: starRes
   }
 }
